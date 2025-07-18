4ed9e1da88e43298183727bed571cd657112a1c5
flosx-talca <orlandorozasi@gmail.com>
2025-07-17 21:35:33 -0400

    Primer commit

---
+++ tests/first_test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+#from webdriver_manager.chrome import ChromeDriverManager    
+
+def test_page_title():
+    browser = webdriver.Chrome() #service=Service("/Users/orozas/Adalid/selenium/chrome/chromedriver")
+    browser.get("https://github.com")
+    
+    titleElement = browser.find_element(By.ID,"hero-section-brand-heading")
+    
+    assert titleElement.text == "Build and ship software on a single, collaborative platform"
+    browser.quit()
