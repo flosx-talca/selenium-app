6c060b568e7fd3ce15fa51faf204d9dc38b95d49
flosx-talca <orlandorozasi@gmail.com>
2025-07-17 21:57:52 -0400

    probando2

--- tests/first_test.py
+++ tests/first_test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 #from webdriver_manager.chrome import ChromeDriverManager    
 
 def test_page_title():
