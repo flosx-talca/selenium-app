8ecd6d65dd34911811baf59d949e2cf6ba72bc96
flosx-talca <orlandorozasi@gmail.com>
2025-07-17 22:26:14 -0400

    test

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-#from webdriver_manager.chrome import ChromeDriverManager    
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_page_title():
-    browser = webdriver.Chrome() #service=Service("/Users/orozas/Adalid/selenium/chrome/chromedriver")
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service, options=chrome_options)
+    
     browser.get("https://github.com")
     
-    titleElement = browser.find_element(By.ID,"hero-section-brand-heading")
-    
+    titleElement = browser.find_element(By.ID, "hero-section-brand-heading")
     assert titleElement.text == "Build and ship software on a single, collaborative platform"
+    
     browser.quit()
