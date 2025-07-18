9945b01a4340286eb6813b4f321f08c317c17efc
flosx-talca <orlandorozasi@gmail.com>
2025-07-17 22:05:08 -0400

    probando4

--- tests/first_test.py
+++ tests/first_test.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 #from webdriver_manager.chrome import ChromeDriverManager    
+from selenium.webdriver.chrome.options import Options
 
 def test_page_title():
-    browser = webdriver.Chrome() #service=Service("/Users/orozas/Adalid/selenium/chrome/chromedriver")
+    options = Options()
+    options.headless = True  # Run in headless mode if desired
+    browser = webdriver.Chrome(options) #service=Service("/Users/orozas/Adalid/selenium/chrome/chromedriver")
     browser.get("https://github.com")
     
     titleElement = browser.find_element(By.ID,"hero-section-brand-heading")
